d2408586c7f5d2d5a8ad04605ef7a0e9aa77daa9
Brienna Macnish <brienna.macnish@envato.com>
2019-11-18 13:20:00 +1100

    Add yaml store to the project to persist voting data

--- vote.rb
+++ vote.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
+require 'yaml/store'
+
+Choices = {
+  'DOG' => 'Dog',
+  'CAT' => 'Cat',
+}
 
 get '/' do
-  'Hello, voter!'
+  @title = 'Which one is cuter?'
+  erb :index
+end
+
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+  erb :cast
+end
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
 end
